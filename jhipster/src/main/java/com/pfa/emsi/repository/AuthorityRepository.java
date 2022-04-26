77ea8fe8dc21d7e9a72c8b1a6a23bcc7cb311b49
Bader19 <badereddinebenhirt@gmail.com>
2022-04-26 02:25:45 +0000

    Initial version of pfa generated by generator-jhipster@7.8.1

---
+++ jhipster/src/main/java/com/pfa/emsi/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.pfa.emsi.repository;
+
+import com.pfa.emsi.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
